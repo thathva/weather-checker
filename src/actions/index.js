1d0ee7b8851838960b58d77033f2752aaf1cd976
Thathva Sri Sai Reddy Y <34968163+thathva@users.noreply.github.com>
2020-07-24 19:26:31 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {KEY} from './key.js'
 export const FETCH_WEATHER = "FETCH_WEATHER"
 
-const URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${KEY}`;
+const URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${KEY}`;
 
 function fetchWeather(city) {
     const url = `${URL}&q=${city}`;
